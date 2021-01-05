7454dbc0f361054b6599d1bcdde54df9daf8de33
Krystian <krystian.izdebski1999@gmail.com>
2021-01-05 14:02:01 +0100

    First task completed

--- src/pl/krystian/MainClass.java
+++ src/pl/krystian/MainClass.java
@@ -3,8 +3,8 @@ package pl.krystian;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
+		System.out.println("Krystian Izdebski");
 	}
 
 }
